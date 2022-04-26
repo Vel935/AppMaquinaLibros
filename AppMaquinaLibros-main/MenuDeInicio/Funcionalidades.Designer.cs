ed6bd8e1fdab66f803257af5b822707ee41bfb11
Vel935 <103478887+Vel935@users.noreply.github.com>
2022-04-25 21:39:42 -0500

    Add files via upload

---
+++ AppMaquinaLibros-main/MenuDeInicio/Funcionalidades.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace MenuDeInicio
+{
+    partial class Funcionalidades
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBPrecio = new System.Windows.Forms.TextBox();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.txtBCantidad = new System.Windows.Forms.TextBox();
+            this.lblCantidad = new System.Windows.Forms.Label();
+            this.txtBCategoria = new System.Windows.Forms.TextBox();
+            this.lblCategoria = new System.Windows.Forms.Label();
+            this.lblLibros = new System.Windows.Forms.Label();
+            this.lstLibrosDisponibles2 = new System.Windows.Forms.ListBox();
+            this.txtBCodigo = new System.Windows.Forms.TextBox();
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.txtBNombre = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnComprar = new System.Windows.Forms.Button();
+            this.btnAcceder = new System.Windows.Forms.Button();
+            this.txtContraseña = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtBPrecio
+            // 
+            this.txtBPrecio.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBPrecio.Enabled = false;
+            this.txtBPrecio.Location = new System.Drawing.Point(31, 279);
+            this.txtBPrecio.Name = "txtBPrecio";
+            this.txtBPrecio.Size = new System.Drawing.Size(100, 22);
+            this.txtBPrecio.TabIndex = 23;
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Location = new System.Drawing.Point(28, 260);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(46, 16);
+            this.lblPrecio.TabIndex = 22;
+            this.lblPrecio.Text = "Precio";
+            // 
+            // txtBCantidad
+            // 
+            this.txtBCantidad.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBCantidad.Enabled = false;
+            this.txtBCantidad.Location = new System.Drawing.Point(31, 224);
+            this.txtBCantidad.Name = "txtBCantidad";
+            this.txtBCantidad.Size = new System.Drawing.Size(100, 22);
+            this.txtBCantidad.TabIndex = 21;
+            // 
+            // lblCantidad
+            // 
+            this.lblCantidad.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblCantidad.AutoSize = true;
+            this.lblCantidad.Location = new System.Drawing.Point(28, 205);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(61, 16);
+            this.lblCantidad.TabIndex = 20;
+            this.lblCantidad.Text = "Cantidad";
+            // 
+            // txtBCategoria
+            // 
+            this.txtBCategoria.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBCategoria.Enabled = false;
+            this.txtBCategoria.Location = new System.Drawing.Point(31, 164);
+            this.txtBCategoria.Name = "txtBCategoria";
+            this.txtBCategoria.Size = new System.Drawing.Size(139, 22);
+            this.txtBCategoria.TabIndex = 19;
+            // 
+            // lblCategoria
+            // 
+            this.lblCategoria.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblCategoria.AutoSize = true;
+            this.lblCategoria.Location = new System.Drawing.Point(28, 145);
+            this.lblCategoria.Name = "lblCategoria";
+            this.lblCategoria.Size = new System.Drawing.Size(66, 16);
+            this.lblCategoria.TabIndex = 18;
+            this.lblCategoria.Text = "Categoria";
+            // 
+            // lblLibros
+            // 
+            this.lblLibros.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblLibros.AutoSize = true;
+            this.lblLibros.Location = new System.Drawing.Point(219, 28);
+            this.lblLibros.Name = "lblLibros";
+            this.lblLibros.Size = new System.Drawing.Size(44, 16);
+            this.lblLibros.TabIndex = 17;
+            this.lblLibros.Text = "Libros";
+            // 
+            // lstLibrosDisponibles2
+            // 
+            this.lstLibrosDisponibles2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstLibrosDisponibles2.FormattingEnabled = true;
+            this.lstLibrosDisponibles2.ItemHeight = 16;
+            this.lstLibrosDisponibles2.Location = new System.Drawing.Point(219, 59);
+            this.lstLibrosDisponibles2.Name = "lstLibrosDisponibles2";
+            this.lstLibrosDisponibles2.Size = new System.Drawing.Size(206, 148);
+            this.lstLibrosDisponibles2.TabIndex = 16;
+            this.lstLibrosDisponibles2.SelectedIndexChanged += new System.EventHandler(this.lstLibrosDisponibles2_SelectedIndexChanged);
+            // 
+            // txtBCodigo
+            // 
+            this.txtBCodigo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBCodigo.Enabled = false;
+            this.txtBCodigo.Location = new System.Drawing.Point(31, 108);
+            this.txtBCodigo.Name = "txtBCodigo";
+            this.txtBCodigo.Size = new System.Drawing.Size(100, 22);
+            this.txtBCodigo.TabIndex = 15;
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Location = new System.Drawing.Point(28, 89);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(51, 16);
+            this.lblCodigo.TabIndex = 14;
+            this.lblCodigo.Text = "Código";
+            // 
+            // txtBNombre
+            // 
+            this.txtBNombre.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBNombre.Enabled = false;
+            this.txtBNombre.Location = new System.Drawing.Point(31, 47);
+            this.txtBNombre.Name = "txtBNombre";
+            this.txtBNombre.Size = new System.Drawing.Size(168, 22);
+            this.txtBNombre.TabIndex = 13;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(28, 28);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(56, 16);
+            this.lblNombre.TabIndex = 12;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAgregar.Enabled = false;
+            this.btnAgregar.Location = new System.Drawing.Point(146, 372);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(100, 37);
+            this.btnAgregar.TabIndex = 24;
+            this.btnAgregar.Text = "Agregar libro";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnEliminar.Enabled = false;
+            this.btnEliminar.Location = new System.Drawing.Point(360, 372);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(98, 37);
+            this.btnEliminar.TabIndex = 25;
+            this.btnEliminar.Text = "Eliminar libro";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSalir.Location = new System.Drawing.Point(515, 437);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(84, 37);
+            this.btnSalir.TabIndex = 27;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGuardar.Enabled = false;
+            this.btnGuardar.Location = new System.Drawing.Point(252, 372);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(102, 37);
+            this.btnGuardar.TabIndex = 28;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnComprar
+            // 
+            this.btnComprar.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnComprar.Location = new System.Drawing.Point(31, 372);
+            this.btnComprar.Name = "btnComprar";
+            this.btnComprar.Size = new System.Drawing.Size(100, 37);
+            this.btnComprar.TabIndex = 29;
+            this.btnComprar.Text = "Comprar";
+            this.btnComprar.UseVisualStyleBackColor = true;
+            this.btnComprar.Click += new System.EventHandler(this.btnComprar_Click);
+            // 
+            // btnAcceder
+            // 
+            this.btnAcceder.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAcceder.Location = new System.Drawing.Point(515, 372);
+            this.btnAcceder.Name = "btnAcceder";
+            this.btnAcceder.Size = new System.Drawing.Size(84, 37);
+            this.btnAcceder.TabIndex = 30;
+            this.btnAcceder.Text = "Acceder";
+            this.btnAcceder.UseVisualStyleBackColor = true;
+            this.btnAcceder.Click += new System.EventHandler(this.btnAcceder_Click);
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtContraseña.Location = new System.Drawing.Point(517, 330);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.Size = new System.Drawing.Size(82, 22);
+            this.txtContraseña.TabIndex = 31;
+            // 
+            // Funcionalidades
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 486);
+            this.Controls.Add(this.txtContraseña);
+            this.Controls.Add(this.btnAcceder);
+            this.Controls.Add(this.btnComprar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtBPrecio);
+            this.Controls.Add(this.lblPrecio);
+            this.Controls.Add(this.txtBCantidad);
+            this.Controls.Add(this.lblCantidad);
+            this.Controls.Add(this.txtBCategoria);
+            this.Controls.Add(this.lblCategoria);
+            this.Controls.Add(this.lblLibros);
+            this.Controls.Add(this.lstLibrosDisponibles2);
+            this.Controls.Add(this.txtBCodigo);
+            this.Controls.Add(this.lblCodigo);
+            this.Controls.Add(this.txtBNombre);
+            this.Controls.Add(this.lblNombre);
+            this.Name = "Funcionalidades";
+            this.Text = "Funcionalidades";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBPrecio;
+        private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.TextBox txtBCantidad;
+        private System.Windows.Forms.Label lblCantidad;
+        private System.Windows.Forms.TextBox txtBCategoria;
+        private System.Windows.Forms.Label lblCategoria;
+        private System.Windows.Forms.Label lblLibros;
+        private System.Windows.Forms.ListBox lstLibrosDisponibles2;
+        private System.Windows.Forms.TextBox txtBCodigo;
+        private System.Windows.Forms.Label lblCodigo;
+        private System.Windows.Forms.TextBox txtBNombre;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnComprar;
+        private System.Windows.Forms.Button btnAcceder;
+        private System.Windows.Forms.TextBox txtContraseña;
+    }
+}
---
+++ AppMaquinaLibros-main/MenuDeInicio/Funcionalidades.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AppMaquinaLibros;
+
+
+namespace MenuDeInicio
+{
+    public partial class Funcionalidades : Form
+    {
+        private List<Libro> Listadodelibros = new List<Libro>();
+        public bool V_Agregarlibro = false;
+        public bool V_Eliminarlibro = false;
+        public bool V_Modificarlibro = false;
+        public Funcionalidades()
+        {
+            InitializeComponent();
+            Libro libro1 = new Libro();
+            libro1.Codigo = "01";
+            libro1.Nombre = "Algebra y trigonometria";
+            libro1.Categoria = "Ciencias basicas";
+            libro1.Cantidad = 10;
+            libro1.Valor = 2000;
+
+            Libro libro2 = new Libro();
+            libro2.Codigo = "02";
+            libro2.Nombre = "Matematicas discretas";
+            libro2.Categoria = "Ciencias basicas";
+            libro2.Cantidad = 15;
+            libro2.Valor = 5000;
+
+            Listadodelibros.Add(libro1);
+            Listadodelibros.Add(libro2);
+            lstLibrosDisponibles2.Items.Add(libro1.Nombre);
+            lstLibrosDisponibles2.Items.Add(libro2.Nombre);
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            txtBNombre.Enabled = true;
+            txtBCodigo.Enabled = true;
+            txtBCategoria.Enabled = true;
+            txtBCantidad.Enabled = true;
+            txtBPrecio.Enabled = true;
+            btnGuardar.Enabled = true;
+            V_Agregarlibro = true;
+            V_Eliminarlibro = false;
+            V_Modificarlibro = false;
+
+            txtBNombre.Text = string.Empty;
+            txtBCodigo.Text = string.Empty;
+            txtBCategoria.Text = string.Empty;
+            txtBCantidad.Text = string.Empty;
+            txtBPrecio.Text = string.Empty;
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+
+            if (V_Agregarlibro is true)
+            {
+                if (txtBNombre.Text != "" && txtBCodigo.Text != "" && txtBPrecio.Text != "" && txtBCantidad.Text != "")
+                {
+                    Libro libro_para_agregar = new Libro();
+                    libro_para_agregar.Nombre = txtBNombre.Text;
+                    libro_para_agregar.Codigo = txtBCodigo.Text;
+                    libro_para_agregar.Categoria = txtBCategoria.Text;
+                    libro_para_agregar.Cantidad = Convert.ToInt32(txtBCantidad.Text);
+                    libro_para_agregar.Valor = Convert.ToDouble(txtBPrecio.Text);
+                    Listadodelibros.Add(libro_para_agregar);
+                    lstLibrosDisponibles2.Items.Add(libro_para_agregar.Nombre);
+                    btnGuardar.Enabled = false;
+                }
+            }
+
+            else if (V_Eliminarlibro is true)
+            {
+                int indiceLibros = lstLibrosDisponibles2.SelectedIndex;
+                Listadodelibros.RemoveAt(indiceLibros);
+                lstLibrosDisponibles2.Items.Remove(lstLibrosDisponibles2.SelectedItem);
+                btnGuardar.Enabled = false;
+                txtBNombre.Text = string.Empty;
+                txtBCodigo.Text = string.Empty;
+                txtBCategoria.Text = string.Empty;
+                txtBCantidad.Text = string.Empty;
+                txtBPrecio.Text = string.Empty;
+            }
+            else if (V_Modificarlibro is true)
+            {
+
+            }
+
+        }
+
+        private void lstLibrosDisponibles2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int indiceLibros = lstLibrosDisponibles2.SelectedIndex;
+            try
+            {
+                txtBNombre.Text = Listadodelibros[(indiceLibros)].Nombre;
+                txtBCodigo.Text = Listadodelibros[(indiceLibros)].Codigo;
+                txtBCategoria.Text = Listadodelibros[(indiceLibros)].Categoria;
+                txtBCantidad.Text = Convert.ToString(Listadodelibros[(indiceLibros)].Cantidad);
+                txtBPrecio.Text = Convert.ToString(Listadodelibros[(indiceLibros)].Valor);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+
+            }
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            V_Agregarlibro = false;
+            V_Eliminarlibro = true;
+            btnGuardar.Enabled = true;
+
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnComprar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int indiceLibros = lstLibrosDisponibles2.SelectedIndex;
+                if (Listadodelibros[(indiceLibros)].Cantidad > 0)
+                {
+                    Listadodelibros[(indiceLibros)].Cantidad -= 1;
+                    txtBCantidad.Text = Convert.ToString(Listadodelibros[(indiceLibros)].Cantidad);
+                    MessageBox.Show("Su compra ha sido exitosa");
+                }
+                else
+                {
+                    MessageBox.Show("Lo siento, no hay más unidades");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Primero Selecciona un articulo!");
+            }
+
+        }
+        
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        public void accesoCompleto()
+        {
+            btnEliminar.Enabled = true;
+            btnAgregar.Enabled = true;
+        }
+
+        private void btnAcceder_Click(object sender, EventArgs e)
+        {
+            if (txtContraseña.Text == "0451")
+            {
+                accesoCompleto();
+            }
+            else txtContraseña.Text = string.Empty;
+
+
+        }
+
+    
+    }
+}
